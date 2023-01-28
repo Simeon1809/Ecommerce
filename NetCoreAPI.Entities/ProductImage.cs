58688f8c59c58eead1a97257e30bc2b690d0b131
Simeon <Simeondigital@gmail.com>
2023-01-28 12:01:11 +0100

    first commit

---
+++ NetCoreAPI.Entities/ProductImage.cs
@@ -0,0 +1,10 @@
+namespace NetCoreAPI.Entities
+{
+    public partial class ProductImage
+    {
+         public int ProductImageId { get; set; }
+
+         public string ImageUrl { get; set; }
+
+    }
+}
